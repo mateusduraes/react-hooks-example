eb289a3e5ce0f24c5f151df8873bac72efb1f783
Mateus Durães <mduraes1994@gmail.com>
2019-08-23 02:29:50 -0300

    use state hook without calling API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
-  return <div>my app</div>
+  const [contacts] = useState([
+    {
+      name: 'Lucas',
+      id: 1,
+    },
+    {
+      name: 'Luís',
+      id: 2,
+    },
+  ])
+
+  return (
+    <div>
+      <h1>Contact list</h1>
+      <ul>
+        {contacts.map(contact => (
+          <li key={contact.id}>{contact.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default App
